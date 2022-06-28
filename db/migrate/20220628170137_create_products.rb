6837ebfa4646177db0256fdffc4cc922753059bb
RubyR0S <fedorovych22@ukr.net>
2022-06-28 20:16:41 +0300

    Create Product migration

---
+++ db/migrate/20220628170137_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.decimal :size
+      t.boolean :is_spicy
+      t.boolean :is_veg
+      t.boolean :is_best_offer
+      t.string :path_to_image
+
+      t.timestamps
+    end
+  end
+end
