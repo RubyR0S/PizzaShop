58e99364f1e69051a5f37b6358e6e6ca11288905
RubyR0S <fedorovych22@ukr.net>
2022-06-29 15:42:32 +0300

    Pass id to add_to_cart

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ function something()
     alert(x);     
  }
 
- function add_to_cart()
+ function add_to_cart(id)
 {
-    alert('Hello from funtion');
+    alert('You addet pizza with id: ' + id);
 }
