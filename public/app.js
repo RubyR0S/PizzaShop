293db387048683ec6e423337bbdd655a4b2fc211
RubyR0S <fedorovych22@ukr.net>
2022-06-29 16:49:49 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,15 +1,19 @@
 function something() 
 {  
-    var x = window.localStorage.getItem("bbb");
+    var x = window.localStorage.getItem('bbb');
 
     x = x * 1 + 1;
 
-    window.localStorage.getItem("bbb" ,x);
+    window.localStorage.setItem('bbb' ,x);
 
     alert(x);     
  }
 
  function add_to_cart(id)
 {
-    alert('You addet pizza with id: ' + id);
+    var key = 'product_' + id;
+
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
 }
