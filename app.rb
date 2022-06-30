72da15a5dddab597c77a47b17946f296c0fd855b
RubyR0S <fedorovych22@ukr.net>
2022-06-30 17:17:36 +0300

    Display total price and total qty

--- app.rb
+++ app.rb
@@ -21,7 +21,12 @@ end
 
 post '/cart' do
 	orders_input = params[:orders]
-	@orders = parse_orders_input orders_input
+	@items = parse_orders_input orders_input
+
+	@items.each do |item|
+		item[0] = Product.find(item[0])
+	end
+
 	erb :cart
 end
 
