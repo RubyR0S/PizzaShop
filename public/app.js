3e744e8d9ca479dbda8e841b3262a5e84a792b4e
RubyR0S <fedorovych22@ukr.net>
2022-06-30 14:46:36 +0300

    Update orders button

--- public/app.js
+++ public/app.js
@@ -18,6 +18,7 @@ function something()
     window.localStorage.setItem(key, x);
 
     update_orders_input();
+    update_orders_button(); 
 }
 
 function cart_get_number_of_items() 
@@ -42,8 +43,13 @@ function update_orders_input()
 {
     var orders = cart_get_orders();
     $('#orders_input').val(orders); 
-}
+} 
 
+function update_orders_button()
+{
+    var text = 'Cart (' + cart_get_number_of_items() + ')';
+    $('#orders_button').val(text); 
+}
 
 function cart_get_orders()
 {
